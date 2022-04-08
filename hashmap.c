08dfe01e236a46d53f6d0fb0a230ca9dd03fc253
SEBASTIAN-ALES1 <>
2022-04-08 01:43:47 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -77,16 +77,12 @@ Pair * searchMap(HashMap * map,  char * key) {
   
   while (map->buckets[pos] != NULL && is_equal(map->buckets[pos]->key,key) == 0){
     pos = ((pos+1)%(map->capacity)); //Para avanzar de posicion como un nextList
-     if(map->buckets[pos]==NULL || map->buckets[pos]->value == NULL)
-    return NULL;
+     if(map->buckets[pos]==NULL || map->buckets[pos]->value == NULL) return NULL;
   }
   
- 
-
   map->current=pos;
   
-  
-    return map->buckets[pos];
+  return map->buckets[pos];
 }
 
 Pair * firstMap(HashMap * map) {
