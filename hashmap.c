f82b458e36d0eb095a4578ceea56b8c339654701
SEBASTIAN-ALES1 <>
2022-04-09 21:58:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -43,8 +43,7 @@ void insertMap(HashMap * map, char * key, void * value) {
   long pos =hash(key,map->capacity);
 
   while (map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
-    if(is_equal(map->buckets[pos]->key, key) == 1)
-      break; 
+    if(is_equal(map->buckets[pos]->key, key) == 1) break; 
     pos = ((pos+1) % map->capacity); //para ir avanzando
   }
   
